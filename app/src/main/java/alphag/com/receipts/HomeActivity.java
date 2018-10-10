4cf1e2d903cb84e07aa8fb02fc4b734d291a78a5
alvarezaaronai <alvarezaaron80@gmail.com>
2018-10-10 01:27:38 -0700

    created the new activity

--- app/src/main/java/alphag/com/receipts/HomeActivity.java
+++ app/src/main/java/alphag/com/receipts/HomeActivity.java
@@ -1,13 +1,30 @@
 package alphag.com.receipts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    //-Member Variables-
+    Button mButton_Camera;
+    //----------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mButton_Camera = findViewById(R.id.camera_button);
+    }
+    /*
+        This method goes to the next Activity, CAMERADETECT.
+     */
+    public void camera_button(View view) {
+        //When button is clicked, goes through all this.
+        //Make a new Intent
+        Intent intent = new Intent(HomeActivity.this, CameraDetect.class);
+        //Start the Intent.
+        startActivity(intent);
     }
 }
