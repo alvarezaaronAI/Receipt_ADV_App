09985c0a2dd5e3aab4f1cb156450f7ab742cd768
alvarezaaronai <alvarezaaron80@gmail.com>
2018-10-14 23:49:03 -0700

    added methods store/use high quality photos

--- app/src/main/java/alphag/com/receipts/CameraDetect.java
+++ app/src/main/java/alphag/com/receipts/CameraDetect.java
@@ -2,9 +2,13 @@ package alphag.com.receipts;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,25 +26,33 @@ import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecogniz
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class CameraDetect extends AppCompatActivity {
-    private static final int REQUEST_TAKE_PHOTO = 0;
     //Member Variables.
     private ImageView mImageView_Camera;
     private Button mButton_Snap;
     private Button mButton_Detect;
     private TextView mTextview_Text;
+    //More Member Variables ( Function Available)
     private Bitmap imageBitmap;
+    String mCurrentPhotoPath;
     //Static Member Variables.
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 1 ;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //On Create
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_detect);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         //Set member variables.
         mImageView_Camera = findViewById(R.id.camera_image);
         mButton_Snap = findViewById(R.id.camera_button_snapshot);
@@ -49,28 +61,67 @@ public class CameraDetect extends AppCompatActivity {
         //---------------------
     }
 
-    /**
-     * This method would allow us to take a picture.
+    /*
+     * This methods will allow us to take a picture.
      * --------------------
      * Notes: Idea - Camera - BitMap Object - Firebase - Detection -Extract
      * */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.getStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "alphag.com.receipts",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
+    //This method will allows to retrieve the photo, to detect text recognition.
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView_Camera.getWidth();
+        int targetH = mImageView_Camera.getHeight();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            mImageView_Camera.setImageBitmap(imageBitmap);
-        }
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView_Camera.setImageBitmap(bitmap);
+    }
+    //This method will allow us to save the current photo to our gallery, using the most recent path.
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
     /*
-        Handles Buttons
+     * Handles Buttons actions
      */
     //This method would handle Button Snapshot.
     public void camera_button_snapshot(View view) {
@@ -80,7 +131,9 @@ public class CameraDetect extends AppCompatActivity {
     public void camera_button_detect(View view) {
             detect_text();
     }
-
+    /*
+     * Handles image recognition
+     */
     private void detect_text() {
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
@@ -98,6 +151,9 @@ public class CameraDetect extends AppCompatActivity {
         });
     }
 
+    /*
+     * Handles image processing to external uses
+     */
     private void process_text(FirebaseVisionText firebaseVisionText) {
         List<FirebaseVisionText.TextBlock> blocks = firebaseVisionText.getTextBlocks();
         if (blocks.size() == 0) {
@@ -113,4 +169,25 @@ public class CameraDetect extends AppCompatActivity {
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
+    /*
+     * Handles High Quality Picture
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Receipt_Date_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    /*
+     * Handles Permissions Methods
+     */
 }
