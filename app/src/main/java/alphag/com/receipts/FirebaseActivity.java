189694433b0bd63285f38b8087ea94e76e4f4815
alvarezaaronai <alvarezaaron80@gmail.com>
2018-11-09 20:16:38 -0800

    added firebase activity basic and utilities

---
+++ app/src/main/java/alphag/com/receipts/FirebaseActivity.java
@@ -0,0 +1,17 @@
+package alphag.com.receipts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirebaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+
+
+    }
+
+    private firebase
+}
