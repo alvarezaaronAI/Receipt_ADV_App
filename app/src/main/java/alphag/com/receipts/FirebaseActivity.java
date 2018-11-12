3d88e430b73d962a7d552b1476f734f9b9486246
alvarezaaronai <alvarezaaron80@gmail.com>
2018-11-12 14:42:42 -0800

    working demo finished

--- app/src/main/java/alphag/com/receipts/FirebaseActivity.java
+++ app/src/main/java/alphag/com/receipts/FirebaseActivity.java
@@ -1,54 +1,37 @@
 package alphag.com.receipts;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseActivity extends AppCompatActivity {
     private static final String TAG = "FirebaseActivity";
-    EditText mET_User_Id;
-    EditText mET_Receipt_Id;
+    TextView mTv_User_Id;
+    TextView mTv_Receipt_Id;
     //This returns a reference of the Json Tree
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference userIdRef = mRootRef.child("users");
+    DatabaseReference mUserIdRef = mRootRef.child("users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
 
-        mET_User_Id = (EditText) findViewById(R.id.et_user_id);
-        mET_Receipt_Id = (EditText) findViewById(R.id.et_receipts_id);
+        mTv_User_Id = (TextView) findViewById(R.id.tv_user_id);
+        mTv_Receipt_Id = (TextView) findViewById(R.id.tv_receipts_id);
     }
 
     public void addUser(String userId , String receiptId) {
-        String userIdTemp = mET_User_Id.getText().toString();
-        String receiptIdTemp = mET_Receipt_Id.getText().toString();
-
+        String userIdTemp = mTv_User_Id.getText().toString();
+        String receiptIdTemp = mTv_Receipt_Id.getText().toString();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        userIdRef.addValueEventListener(new ValueEventListener() {
-            //When ever the Users Id Database gets updated
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String userId = dataSnapshot.getValue(String.class);
-                mET_User_Id.setText(userId);
-            }
-            //When ever theres an error
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        //Test user Id = 1234567890
+        DatabaseReference mReceipts = mUserIdRef.child("rec");
     }
 }
