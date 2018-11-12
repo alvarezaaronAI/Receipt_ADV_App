c59e81e02f5b2afe2cc632a877193e0c20c2d3db
alvarezaaronai <alvarezaaron80@gmail.com>
2018-11-12 13:07:50 -0800

    added firebase functionality demo

--- app/src/main/java/alphag/com/receipts/FirebaseActivity.java
+++ app/src/main/java/alphag/com/receipts/FirebaseActivity.java
@@ -1,14 +1,23 @@
 package alphag.com.receipts;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseActivity extends AppCompatActivity {
     private static final String TAG = "FirebaseActivity";
     EditText mET_User_Id;
     EditText mET_Receipt_Id;
-    
+    //This returns a reference of the Json Tree
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference userIdRef = mRootRef.child("users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +33,22 @@ public class FirebaseActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        userIdRef.addValueEventListener(new ValueEventListener() {
+            //When ever the Users Id Database gets updated
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String userId = dataSnapshot.getValue(String.class);
+                mET_User_Id.setText(userId);
+            }
+            //When ever theres an error
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
--- app/src/main/java/alphag/com/receipts/HomeActivity.java
+++ app/src/main/java/alphag/com/receipts/HomeActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HomeActivity extends AppCompatActivity {
     //-Member Variables-
     Button mButton_Camera;
+    Button mButton_Firebase;
     //----------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         mButton_Camera = findViewById(R.id.camera_button);
+        mButton_Firebase = findViewById(R.id.firebase_button);
     }
     /*
         This method goes to the next Activity, CAMERADETECT.
@@ -24,6 +28,12 @@ public class HomeActivity extends AppCompatActivity {
         //When button is clicked, goes through all this.
         //Make a new Intent
         Intent intent = new Intent(HomeActivity.this, CameraDetect.class);
+        //Start the Intent.
+        startActivity(intent);
+    }
+
+    public void firebase_button(View view) {
+        Intent intent = new Intent(HomeActivity.this, FirebaseActivity.class);
         //Start the Intent.
         startActivity(intent);
     }
