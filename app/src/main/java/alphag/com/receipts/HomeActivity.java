f2b80e42b8cdaf10d16eeea14f13b5d56d55ed9c
epoxi117 <gplacid2@calstatela.edu>
2018-11-18 12:40:34 -0800

    Added linear layout within relative layout for viewholder

--- app/src/main/java/alphag/com/receipts/HomeActivity.java
+++ app/src/main/java/alphag/com/receipts/HomeActivity.java
@@ -3,20 +3,24 @@ package alphag.com.receipts;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
     //-Member Variables-
-    ImageButton mButton_Camera;
+    Button mButton_Camera;
+    String TAG = "Camera button";
     //----------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mButton_Camera = (ImageButton) findViewById(R.id.camera_button);
+        Log.d(TAG, "camera activate ");
+
+        mButton_Camera =  findViewById(R.id.camera_button);
     }
     /*
         This method goes to the next Activity, CAMERADETECT.
