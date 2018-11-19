dd9a166e5b983d12e2930093bcfb9b3f40dfb46c
matthewramos05 <matthewhramos@gmail.com>
2018-11-19 11:27:31 -0800

    saved changes

---
+++ app/src/main/java/alphag/com/receipts/UserHomeActivity.java
@@ -0,0 +1,13 @@
+package alphag.com.receipts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userhome);
+    }
+}
