4ec83e0fbd326271fd7eff020314cba104f297d6
ggranad4 <gerardogranados45@yahoo.com>
2018-12-11 02:13:04 -0800

    Permissionsv2

--- app/src/main/java/alphag/com/receipts/Maps/ReceiptsMapsActivity.java
+++ app/src/main/java/alphag/com/receipts/Maps/ReceiptsMapsActivity.java
@@ -2,21 +2,32 @@ package alphag.com.receipts.Maps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.util.MapUtils;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
+import java.io.IOException;
+import java.util.List;
 
 import alphag.com.receipts.R;
 
@@ -36,7 +47,7 @@ public class ReceiptsMapsActivity extends FragmentActivity implements OnMapReady
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setMarkers("7122 Arbutus Ave huntington Park 90255");
         setContentView(R.layout.activity_receipts_maps2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -80,6 +91,61 @@ public class ReceiptsMapsActivity extends FragmentActivity implements OnMapReady
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
+    }
+    public void setMarkers(String location){
+        Geocoder gc = new Geocoder(ReceiptsMapsActivity.this);
+        try {
+            List<Address> fin =  gc.getFromLocationName(location, 3);
+
+            Address fin1 = fin.get(0);
+            String cal = fin1.getLocality();
+
+            double lat = fin1.getLatitude();
+            double log = fin1.getLongitude();
+
+            LatLng latlng = new LatLng(lat,log);
+            MarkerOptions options = new MarkerOptions().position(latlng).title("Spot");
+            mMap.addMarker(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void getDeviceLocation(){
+        Log.d(TAG, "InitMap: initializing map" );
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if(mLocationPermissionsGranted){
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "OnComplete: Found Location");
+                            Location currentLocation = (Location)task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM, "My Location");
+
+                        }
+                        else{
+                            Log.d(TAG, "OnCOmplete: Unable to get location");
+
+                        }
+                    }
+                });
+            }
+
+        }catch (SecurityException e){
+            Log.e(TAG, "GetDeviceLocation: SecurityException " + e.getMessage());
+        }
+    }
+    private void moveCamera(LatLng latlng, float zoom, String title){
+        Log.d(TAG, "MoveCamera: Moving the camera to: lat:" + latlng.latitude + ", lng:" + latlng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,zoom) );
+
+        MarkerOptions options = new MarkerOptions().position(latlng).title(title);
+
+        mMap.addMarker(options);
+        setMarkers("7122 Arbutus Ave Huntington park 90255");
     }
 
 
